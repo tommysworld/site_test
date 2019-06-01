a2c34b57bdf8ee9c461fd5c38447d4ed6f2a3391
Loïc Bourdain <loic.bourdain@gmail.com>
2019-06-01 15:05:20 +0200

    Prise en compte des includes
    Prise en compte des includes pour le menu, le logo et le pied de page

--- inc/logo.inc.php
+++ inc/logo.inc.php
@@ -1,1 +1,1 @@
-<a href="index.php" class="logo"><img class="logo" src="img/logo-artiste.png" title="Logo de l'artiste et retour à la page d'acceuil" alt="Logo de l'artiste et retour à la page d'acceuil"/></a>
+<a href="index.php" class="logo"><img class="logo" src="img/logo-artiste.png" title="Logo de l'artiste et retour à la page d'accueil" alt="Logo de l'artiste et retour à la page d'accueil"/></a>
